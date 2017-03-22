dbd816a72ab70e01e66a11b1658f2d8fdefee094
A. Omar <omara@hr.nl>
2017-03-22 18:58:09 +0100

    v2 completed

---
+++ InClassDemoGame/InClassDemoGame/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace InClassDemoGame
+{
+	public class Ball : IComponent
+	{
+		Texture2D ball;
+		Vector2 position;
+		IStateMachine process;
+
+		public Ball (Texture2D b ,Vector2 pos)
+		{
+			this.ball = b;
+			this.position = pos;
+
+			//Define the behavior of the object ball through the statemachine
+			this.process = new Repeat (new Seq (new Timer (0.5f), new Call (() => this.position.X += this.position.X * 0.5f)));
+			//this.process = new Repeat (new Seq (new Timer(0.5f), new CallIf (()=> this.position.X < 500 , () => this.position.X += this.position.X * 0.5f)));
+
+		}
+
+		public void Draw (SpriteBatch spriteBatch)
+		{
+			//Draw the texture at a the given position  
+			spriteBatch.Draw (ball, position);
+		}
+
+		public void Update (float dt)
+		{
+			//Update the statemachines 
+			this.process.Update (dt);
+
+		}
+	}
+}
---
+++ InClassDemoGame/InClassDemoGame/GameState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace InClassDemoGame
+{
+	class GameState : IComponent
+	{
+		Ball ball1;
+		Ball ball2;
+
+
+		public GameState (Texture2D b1, Texture2D b2)
+		{
+			//Create two ball instances, that have a texture and a position 
+			ball1 = new Ball (b1, new Vector2(10, 10)); 
+			ball2 = new Ball( b2, new Vector2(30, 30));
+		}
+
+		public void Draw (SpriteBatch spriteBatch)
+		{
+			//Draw both balls
+			ball1.Draw (spriteBatch);
+			ball2.Draw (spriteBatch);
+		}
+
+		public void Update (float dt)
+		{
+			//Update both balls
+			ball2.Update (dt);
+			ball1.Update (dt);
+		}
+	}
+}
